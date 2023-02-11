8e2dfffe6cc2a750fbc2693e118240291227ff8d
Royu.Chan <w983874@gmail.com>
2023-02-11 23:42:55 +0800

    add Developer-Skill Coding Challenge #1

--- 03-Developer-Skills/practice/script.js
+++ 03-Developer-Skills/practice/script.js
@@ -1,3 +1,21 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// Coding Challenge #1
+
+// Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = (arr) => {
+  let messages = "";
+  for (let i = 0; i < arr.length; i++) {
+    const inDays = i + 1;
+    messages += `...${arr[i]}C in ${inDays} days`;
+  }
+  console.log(`${messages}`);
+};
+
+console.log(printForecast(data1));
+console.log(printForecast(data2));
